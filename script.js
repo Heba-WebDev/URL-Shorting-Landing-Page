8c8b2c97b4a2e1a98774d3a4c9d983ce9265f5f1
Heba-Webdev23 <jtvonline@yahoo.com>
2022-01-03 21:29:02 +0300

    updated the navbar but not yet done with it

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 
 
+let mobileMenu = document.querySelector('.mobile-menu');
+mobileMenu.style.display = 'none';
+
+let mobileHamburgerIcon = document.querySelector('.fa-bars');
+
+mobileHamburgerIcon.addEventListener('click', showMobileMenu);
+
+function showMobileMenu() {
+
+    if(mobileMenu.dataset.mobileMenu == "false") {
+        mobileMenu.style.display = '';
+        mobileMenu.dataset.mobileMenu = "true"
+    } else {
+        mobileMenu.style.display = 'none';
+        mobileMenu.dataset.mobileMenu = "false"
+    }
+    
+}
